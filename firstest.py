4b4a7cd118f9695c7663bcd1d869c891d2851156
Ishita <ishitasingh5688@gmail.com>
2025-05-26 15:04:42 +0400

    Created new variables and added more info

--- firstest.py
+++ firstest.py
@@ -1,1 +1,5 @@
 print("Hello Ishita --- First time joining me?")
+print("I finally pushed in the .py and .txt file")
+age = 17
+name = "Ishita"
+print("My intro: My name is", name, "and I am", age, "years old") 
