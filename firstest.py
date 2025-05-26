256a6e599c24410825800c2c97ab385bd749b97a
Ishita <ishitasingh5688@gmail.com>
2025-05-26 15:16:41 +0400

    Created driving test criteria

--- firstest.py
+++ firstest.py
@@ -1,5 +1,7 @@
-print("Hello Ishita --- First time joining me?")
-print("I finally pushed in the .py and .txt file")
-age = 17
-name = "Ishita"
-print("My intro: My name is", name, "and I am", age, "years old") 
+print("Hello Ishita --- So Good to See you Again!")
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+if age > 18:
+    print(name,",you are eligible to take driving classes!")
+else:
+    print(name,",you are currently underage. Sorry!")
